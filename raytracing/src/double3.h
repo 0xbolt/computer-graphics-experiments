c3a66ec771c1dee5f5405af75d34bcdc9fc99c7f
0xpapercut <0xpapercut@gmail.com>
2025-06-27 22:34:52 +0200

    Add double3.h

---
+++ raytracing/src/double3.h
@@ -0,0 +1,33 @@
+#pragma once
+
+class alignas(16) double3 {
+public:
+    double x, y, z;
+
+    double3() : x(0), y(0), z(0), pad(0) { }
+    double3(double x, double y, double z) : x(x), y(y), z(z) { }
+
+    double3 operator-() const { return double3(-x, -y, -z); }
+    double3 operator+(const double3& v) const {
+        return double3(x + v.x, y + v.y, z + v.z);
+    }
+    double3& operator+=(const double3& v) {
+        x += v.x;
+        y += v.y;
+        z += v.z;
+        return *this;
+    }
+    double3& operator*=(double t) {
+        x *= t;
+        y *= t;
+        z *= t;
+        return *this;
+    }
+    double3& operator/=(double t) {
+        *this *= 1 / t;
+        return *this;
+    }
+
+private:
+    double pad;
+};
